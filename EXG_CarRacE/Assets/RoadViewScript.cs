dddbbc280c6184dc808d073035f3cd357d875dc9
Shubham Kastiya <kastiya.shubham@gmail.com>
2021-11-01 01:57:17 +0000

    Assets update 2

---
+++ EXG_CarRacE/Assets/RoadViewScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadViewScript : MonoBehaviour
+{
+    [SerializeField] GameObject GameHandle;
+    [SerializeField] GameObject RoadCameraView;
+
+    bool isActive = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        RoadCameraView.SetActive(true);
+        GameHandle.SetActive(false);
+        FindObjectOfType<AudioManager>().Play("CrowdSound");
+
+        FindObjectOfType<AudioManager>().Play("Claps");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isActive == true)
+        {
+            RoadCameraView.SetActive(false);
+            GameHandle.SetActive(true); 
+            isActive = false;
+        }
+    }
+}
